e270da8def44d7f7bb4b30c121680d31078b2d9d
worm704 <109051047@live.asia.edu.tw>
2023-01-10 04:38:52 +0800

    置入鑰匙

--- Unity_Finalllll/Unity_Final_109051047/Assets/Scripts/InteractableSystem.cs
+++ Unity_Finalllll/Unity_Final_109051047/Assets/Scripts/InteractableSystem.cs
@@ -1,18 +1,80 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
+using UnityEngine.Events;
 
-public class InteractableSystem : MonoBehaviour
+namespace Nick
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    /// <summary>
+    /// 互動系統:偵測玩家是否進入觸發區域並處理互動行為
+    /// </summary>
 
-    // Update is called once per frame
-    void Update()
+
+    public class InteractableSystem : MonoBehaviour
     {
-        
+        [SerializeField, Header("第一段對話資料")]
+        private DialogueData dataDialogue;
+        [SerializeField, Header("對話結束後的事件")]
+        private UnityEvent onDialogueFinish;
+
+        [SerializeField, Header("啟動道具")]
+        private GameObject propActive;
+        [SerializeField, Header("啟動後的對話資料")]
+        private DialogueData dataDialogueActive;
+        [SerializeField, Header("啟動後對話結束後的事件")]
+        private UnityEvent onDialogueFinishAfterActive;
+
+
+        private string nameTarget = "PlayerCapsule";
+        private DialogueSystem dialogueSystem;
+
+        private void Awake()
+        {
+            dialogueSystem = GameObject.Find("畫布對話系統").GetComponent<DialogueSystem>();
+        }
+        // 兩個碰撞物件碰撞
+        // 其中一個有勾選 Is Trigger
+        // 觸發開始
+        private void OnTriggerEnter(Collider other)
+        {
+            //如果 碰撞物件名稱 包含 PlayerCapsule 就執行  { }
+            if (other.name.Contains(nameTarget))
+            {
+                print(other.name);
+
+                // 如果 不需要啟動道具 或者 啟動道具是顯示狀態(還沒拿) 就執行第一段對話
+                if (propActive == null || propActive.activeInHierarchy)
+                {
+                    dialogueSystem.StartDialogue(dataDialogue, onDialogueFinish);
+                }
+                else
+                {
+                    dialogueSystem.StartDialogue(dataDialogueActive, onDialogueFinishAfterActive);
+
+                }
+
+            }
+        }
+
+        // 觸發離開
+        private void OnTriggerExit(Collider other)
+        {
+
+        }
+
+        // 觸發持續
+        private void OnTriggerStay(Collider other)
+        {
+
+        }
+
+
+        /// <summary>
+        /// 隱藏物件
+        /// </summary>
+        public void HiddenObject()
+        {
+            gameObject.SetActive(false);
+        }
+
     }
 }
--- Unity_Finalllll/Unity_Final_109051047/Assets/Scripts/SoundSystem.cs
+++ Unity_Finalllll/Unity_Final_109051047/Assets/Scripts/SoundSystem.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class SoundSystem : MonoBehaviour
+namespace MyNamespace
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// 音效系統
+    /// </summary>
+    ///要求元件:在第一次套用此腳本時會添加裡面指定的元件
+    [RequireComponent(typeof(AudioSource))]
+    public class SoundSystem : MonoBehaviour
     {
-        
-    }
+        private AudioSource aud;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Awake()
+        {
+            aud = GetComponent<AudioSource>();
+        }
+
+        /// <summary>
+        /// 播放音效
+        /// </summary>
+        /// <param name="sound">要播放的音效</param>
+        public void PlaySound(AudioClip sound)
+        {
+            // 音效來源.撥放一次音效{音效};
+            aud.PlayOneShot(sound);
+        }
+
+
     }
 }
